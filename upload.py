9397b57a35d888f42b226922043bb1c9776c7061
mooping88 <94504896+mooping88@users.noreply.github.com>
2023-01-26 15:09:51 +0700

    Update upload.py

--- upload.py
+++ upload.py
@@ -1,10 +1,10 @@
 import streamlit as st
-import pandas as pd
-from io import StringIO
+import cv2 as cv
+import tempfile
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
     st.write(bytes_data)
-    
+    vf = cv.VideoCapture(tfile.name)
