a4bf53f955aed33ec3468767ff911ca40202f2b9
mooping88 <94504896+mooping88@users.noreply.github.com>
2023-01-26 14:53:41 +0700

    Update upload.py

--- upload.py
+++ upload.py
@@ -6,11 +6,11 @@ if uploaded_file is not None:
     vf = cv.VideoCapture(uploaded_file)
     
     while vf.isOpened():
-    ret, frame = vf.read()
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    stframe.image(gray)
+        ret, frame = vf.read()
+        # if frame is read correctly ret is True
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        stframe.image(gray)
     
