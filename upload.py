5178220176fdb17e2444f64b9e72995b42703286
mooping88 <94504896+mooping88@users.noreply.github.com>
2023-01-26 19:19:21 +0700

    Update upload.py

--- upload.py
+++ upload.py
@@ -3,4 +3,9 @@ import streamlit as st
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-    st.video(uploaded_file, format="video/mp4", start_time=0)
+   # st.video(uploaded_file, format="video/mp4", start_time=0)
+    video_file = open(uploaded_file, 'rb') #enter the filename with filepath
+
+    video_bytes = video_file.read() #reading the file
+
+    st.video(video_bytes) #displaying the video
