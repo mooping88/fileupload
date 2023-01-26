61548227260dc85d3c91492d9d15672313baa5d4
mooping88 <94504896+mooping88@users.noreply.github.com>
2023-01-26 15:37:19 +0700

    Update upload.py

--- upload.py
+++ upload.py
@@ -3,7 +3,4 @@ import streamlit as st
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-    video_file = open('uploaded_file', 'rb')
-    video_bytes = video_file.read()
-
-    st.video(video_bytes)
+    st.video(uploaded_file, format="video/mp4", start_time=0)
